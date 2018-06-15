891d7c0bc9ecd3287c7b0848792454f5cc96f7e5
Paul Brower <pabrower@icloud.com>
2018-06-15 11:25:44 -0700

    completed checkpoint create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,42 +5,58 @@ class RoomList extends Component {
     super(props);
     this.state = {
       rooms: [],
+      nameNewRoom: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
-  componentDidMount() {
-  	this.roomsRef.on('child_added', snapshot => {
-  	const room = snapshot.val();    
-    this.setState({ rooms: this.state.rooms.concat( room ) });
-    	});
-  }
+componentDidMount() {
+	this.roomsRef.on('child_added', snapshot => {
+	const room = snapshot.val();    
+  this.setState({ rooms: this.state.rooms.concat( room ) });
+  	});
+}
 
-createRoom(){
-  this.roomsRef.push({
-  name: newRoomName //new room name is input text in Form 
- });
+handleChange(e) {
+  this.setState({nameNewRoom: e.target.value});
+}
+
+handleRoomSubmit(e){
+  if (this.state.nameNewRoom) {
+    this.setState({nameNewRoom: ''});
+    this.roomsRef.push({name: this.state.nameNewRoom,});
+  }
 }
 
 render() {
     return (
-      <section className='roomlist'>
-	       {this.state.rooms.map ((room, index) => 
-	         <li key={index}>
-	           {room.name}
-	         </li> 
-           )
-         }
+      <section className="roomCreation">
+         <section id="room-submit">
+           <form onSubmit={ (e) => this.handleRoomSubmit(e)}>
+              <label for="createChatRoom">Create New Room </label>
+              <input 
+                type="text" 
+                value={this.state.nameNewRoom}
+                placeholder="enter room name"
+                onChange={this.handleChange.bind(this)}
+                />
+              <input 
+                type="submit"
+                value="submit"
+                />
+           </form>
+         </section>
+         <section id="room-list">
+           {this.state.rooms.map ((room, index) => 
+             <li key={index}>
+               {room.name}
+             </li> 
+             )
+           }
+         </section>
        </section>
-       <div className='create-room-form'>
-        <form action="" method="post">
-          <fieldset>
-            <input type="text" id="createChatRoom">
-            <label for="createChatRoom">New Room</label>
-            <input type="submit" value="Submit"> 
-          </fieldset>
-        </form>
-       </div>
     );
   }
 }
