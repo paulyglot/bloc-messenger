71e94102e6297699886d183e250372cbc95a0a9e
Paul Brower <pabrower@icloud.com>
2018-06-18 11:34:01 -0700

    added some foundational code to MessageList

---
+++ src/components/MessageList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    render() {
+    return (
+      <section>
+       </section>
+    );
+  }
+}
+
+export default MessageList;
