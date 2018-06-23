9c098229c4aeba03867b99f2c31e9e18ad3a6d1c
Paul Brower <pabrower@icloud.com>
2018-06-23 12:58:36 -0700

    adding css and styling/bootstrap

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
     return (
       <section>
         <nav>
-          <h1>Bloc Messenger</h1>
+          <h1 className="App-header">Bloc Messenger</h1>
           <User firebase={ firebase } setUser={this.setUser} user={ this.state.activeUser } />
         </nav>
         { this.state.activeUser ? 
